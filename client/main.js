9eea082f6e57ebb2e9202a3d394d322069618bc3
Fabio Arpires <arpirez.fabio@gmail.com>
2020-04-11 02:00:39 -0300

    commit: Chat Socket.io y NodeJS

---
+++ client/main.js
@@ -0,0 +1,19 @@
+// alert('hols');
+var socket = io.connect('http://192.168.1.18:6677',{'forceNew':true});
+socket.on('messages',function(data){
+    console.log(data);
+    render(data);
+});
+
+function render(data){
+var html = data.map(function(message, index){
+    return(`
+    <div class="message">
+        <strong>${message.nickname}</strong>dice:
+        <p>${message.text}</p>
+    </div>
+    `);
+}).join(' ');
+
+document.getElementById('messages').innerHTML = html;
+}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,26 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);//esta variable serve se la tengo que psar a socket.io---para que entienda que va a estra escuchando
 var io = require('socket.io')(server);//ESTO ES UNA CONFIGURACION BASICA QUE NECESITAMOS TENER  SINO NO FUNCIONARA NADA DE OS SOQUES 
+//Midwerll
+app.use(express.static('client'));
+
 
 //ruta
 app.get('/hola-mundo',function(req,res){
     res.status(200).send('Hola mundo desde una ruta');
-})
+});
+
+var messages = [{
+    id:1,
+    text:'Bienvenido al chat privado  de Socket.io y Node.js de Fabio Arpires ',
+    nickname: 'Farpach'
+}]
+//on METDOD nos permite lansar eventos
+io.on('connection', function(socket){
+console.log("El cliente con Ip:"+socket.handshake.address+"se ha conectado...")
+socket.emit('messages',messages)
+});
+
 
 //cramos servido con express y lo ponemos a escuchar(listem)
 server.listen(6677, function(){
